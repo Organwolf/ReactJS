4b898ff0951c812e50c12814522f557c7a497d37
Aron Polner <aron.polner@gmail.com>
2020-03-11 15:58:27 +0100

    Added a dependency to react-bootstrap

--- intro-react/src/App.js
+++ intro-react/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import TodoApp from './containers/TodoApp';
 
+// https://react-bootstrap.github.io/getting-started/introduction
+
 class App extends Component {
 
   render() {
