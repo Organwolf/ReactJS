6bc5f62009f0bbc89ff354edd17ac1085d0db4cf
Organwolf <aron.polner@gmail.com>
2020-04-04 16:14:30 +0200

    Init commit with client and server communicating

---
+++ fullstack-blog/Client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ContextState from './context_state_config';
+
+
+
+const App = () => {
+
+    return(
+      <div>
+      React - flap that flap app
+      <ContextState />
+      </div>
+    )
+}
+
+
+export default App;
