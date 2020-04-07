c450117123265d79d13e7e90d75df3e138b50452
Organwolf <aron.polner@gmail.com>
2020-04-07 19:04:45 +0200

    A bit broken on the server side. tsvector is nonexistant

--- fullstack-blog/Client/src/hooks/home.js
+++ fullstack-blog/Client/src/hooks/home.js
@@ -2,17 +2,17 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 
 const Home = props => {
-    useEffect(() => {
-      axios.get('/api/hello')
-        .then(res => setState(res.data))
-    }, [])
+    // useEffect(() => {
+    //   axios.get('/api/hello')
+    //     .then(res => setState(res.data))
+    // }, [])
 
-    const [state, setState] = useState('')
+    // const [state, setState] = useState('')
 
   return(
     <div>
       Home
-      <p>{state}</p>
+      {/* <p>{state}</p> */}
     </div>
  )
 };
