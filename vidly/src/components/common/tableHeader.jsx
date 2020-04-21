9b13986a158e3436f69501da349d8b2bae18c497
Organwolf <aron.polner@gmail.com>
2020-04-21 18:09:00 +0200

    At the end of 23

--- vidly/src/components/common/tableHeader.jsx
+++ vidly/src/components/common/tableHeader.jsx
@@ -16,6 +16,13 @@ class TableHeader extends Component {
     this.props.onSort(sortColumn);
   };
 
+  renderSortIcon = (column) => {
+    const { sortColumn } = this.props;
+    if (column.path !== sortColumn.path) return null;
+    if (sortColumn.order === "asc") return <i className="fa fa-sort-asc"></i>;
+    return <i className="fa fa-sort-desc"></i>;
+  };
+
   render() {
     return (
       <thead>
@@ -27,7 +34,7 @@ class TableHeader extends Component {
                 this.raiseSort(column.path);
               }}
             >
-              {column.label}
+              {column.label} {this.renderSortIcon(column)}
             </th>
           ))}
         </tr>
