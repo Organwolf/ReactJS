2979b5a35ff0b40352ca895a54637eb99efd137e
Organwolf <aron.polner@gmail.com>
2020-04-28 20:40:14 +0200

    At the end of vid 16 backend services

--- http-app/src/index.js
+++ http-app/src/index.js
@@ -1,9 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-import "./index.css";
-import "bootstrap/dist/css/bootstrap.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import logService from './services/logService';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+logger.init();
+
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
---
+++ http-app/src/services/logService.js
@@ -0,0 +1,20 @@
+import Raven from 'raven-js';
+
+function init() {
+  Raven.config(
+    'https://4f88cb0450994f2cb2c333b9e1f62fc1@o384686.ingest.sentry.io/5216286',
+    {
+      release: '1-0-0',
+      environment: 'development-test'
+    }
+  ).install();
+}
+
+function log(error) {
+  Raven.captureException(error);
+}
+
+export default {
+  init,
+  log
+};
