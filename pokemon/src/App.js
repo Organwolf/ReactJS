2c224738cbf1ececc762e79cdebe855755d003ba
Organwolf <aron.polner@gmail.com>
2020-05-04 19:28:21 +0200

    Working after solving two buggs related to useEffect

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -1,19 +1,20 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
 
-import PokemonList from "./PokemonList";
-import CapturedPokemon from "./CapturedPokemon";
-import { PokemonProvider } from "./PokemonProvider";
+import { PokemonProvider } from './Pokemon/PokemonContext';
+import PokemonsList from './Pokemon/PokemonsList';
+import Pokedex from './Pokemon/Pokedex';
+import PokemonForm from './Pokemon/PokemonForm';
 
-function App() {
-  return (
-    <PokemonProvider>
-      <div className="App">
-        <PokemonList />
-        <CapturedPokemon />
-      </div>
-    </PokemonProvider>
-  );
-}
+const App = () => (
+  <PokemonProvider>
+    <div className="main">
+      <PokemonsList />
+      <Pokedex />
+    </div>
+    <div className='form-wrapper'>
+      <PokemonForm />
+    </div>
+  </PokemonProvider>
+);
 
 export default App;
